95990ff5d73bce1094644a2eb5cdc27da9899bed
SLIMBOOK TEAM <sat@slimbook.es>
2017-06-01 14:10:57 +0200

    Update install-for-dummies.sh

--- install-for-dummies.sh
+++ install-for-dummies.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+#no comments, please
+
 cd /tmp/
 
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.149_all.deb     
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.157.10_all.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.161_all.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.164_all.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.165_all.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.79_all.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.79.18_all.deb
+wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.149_all.deb
+dpkg -i /tmp/linux-firmware_1.149_all.deb
 
-dpkg -i /tmp/linux-firmware*.deb
+wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.157.10_all.deb
+dpkg -i /tmp/linux-firmware_1.157.10_all.deb
+
+wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.161_all.deb
+dpkg -i /tmp/linux-firmware_1.161_all.deb
+
+wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.164_all.deb
+dpkg -i /tmp/linux-firmware_1.164_all.deb
+
+wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.165_all.deb
+dpkg -i /tmp/linux-firmware_1.165_all.deb
+
+wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.79_all.deb
+dpkg -i /tmp/linux-firmware_1.79_all.deb
+
+wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.79.18_all.deb
+dpkg -i /tmp/linux-firmware_1.79.18_all.deb
+
+dpkg --configure -a
