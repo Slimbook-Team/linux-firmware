8d14f344d9cc569bf086d5d7c1ff480e85f6ab2b
SLIMBOOK TEAM <sat@slimbook.es>
2017-06-02 14:59:02 +0200

    Update install-for-dummies.sh

--- install-for-dummies.sh
+++ install-for-dummies.sh
@@ -2,7 +2,19 @@
 
 #no comments, please
 
-cd /tmp/
+apt-get install git -y
+rm -rf /tmp/iwlwifi
+mkdir /tmp/iwlwifi
+cd /tmp/iwlwifi/
+echo "Descargando datos del firmware..."
+git clone https://github.com/slimbook/iwlwifi
+cd /tmp/iwlwifi/iwlwifi/
+echo "Ficheros descargados."
+sudo cp iwlwifi-*.ucode /lib/firmware/
+
+echo "Firmware manualmente actualizado.\n"
+
+echo "Ahora si derivados DEB.\n"
 
 wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_1.149_all.deb
 dpkg -i /tmp/linux-firmware_1.149_all.deb
@@ -20,3 +32,8 @@ wget http://mirrors.kernel.org/ubuntu/pool/main/l/linux-firmware/linux-firmware_
 dpkg -i /tmp/linux-firmware_1.165_all.deb
 
 dpkg --configure -a
+
+
+echo "Fin derivados DEB.\n"
+
+echo "Gracias por confiar en el Slimbook Team."
